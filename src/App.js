70724b8dfee1ef4fddf16e7b91d33ffe9b16030f
martuahutapea <mhutapea2708@gmail.com>
2024-06-02 01:41:36 +0700

    Folder and files Structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./containers/home";
+import Contact from "./containers/contact";
+import About from "./containers/about";
+import Projects from "./containers/projects";
+import Resume from "./containers/resume";
+import NotFound from "./containers/NotFound";
+import Navbar from "./components/navbar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {/* Particles js */}
+
+      {/* Navbar */}
+      <Navbar />
+
+      {/* Main page content */}
+      <Routes>
+        <Route index path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import { FaBars, FaReact } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { HiX } from "react-icons/hi";
+
+// Dummy data
+const data = [
+  {
+    label: "Home",
+    to: "/",
+  },
+  {
+    label: "About",
+    to: "/about",
+  },
+  {
+    label: "Projects",
+    to: "/projects",
+  },
+  {
+    label: "Resume",
+    to: "/resume",
+  },
+  {
+    label: "Contact",
+    to: "/contact",
+  },
+];
+
+const Navbar = () => {
+  const [toggleIcon, setToggleIcon] = useState(false);
+  const handleToggleIcon = () => {
+    setToggleIcon(!toggleIcon);
+  };
+  return (
+    <div>
+      <nav className="navbar">
+        <div className="navbar__container">
+          <Link to={"/"} className="navbar_container_logo">
+            <FaReact size={30} />
+          </Link>
+        </div>
+        <ul className="navbar_container_menu">
+          {data.map((item, key) => (
+            <li key={key} className="navbar_container_menu_item">
+              <Link className="navbar_container_menu_item_link" to={item.to}>
+                {item.label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <div className="nav_icon" onClick={handleToggleIcon}>
+          {toggleIcon ? <HiX size={30} /> : <FaBars si size={30} />}
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;

57a488939649a710a4cd14742bfdff2650b646f2
martuahutapea <mhutapea2708@gmail.com>
2024-06-28 05:52:28 +0700

    add the link to the footer and open in another tab

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -5,10 +5,10 @@ import "./styles.scss";
 const Footer = () => {
   const currentYear = new Date().getFullYear();
   const socialMediaLinks = [
-    { icon: "facebook-f", href: "#" },
-    { icon: "instagram", href: "#" },
-    { icon: "linkedin-in", href: "#" },
-    { icon: "github", href: "#" },
+    { icon: "facebook-f", href: "https://www.facebook.com/sudung.hutapea.9?mibextid=ZbWKwL " },
+    { icon: "instagram", href: "https://www.instagram.com/hasudungan_hutapea?igsh=MWptZzNiZGs3djFsZg== " },
+    { icon: "linkedin-in", href: "https://www.linkedin.com/in/martua-ibrahim-6b58a31b2/" },
+    { icon: "github", href: "https://github.com/martuahutapea" },
   ];
 
   return (
@@ -16,7 +16,7 @@ const Footer = () => {
       <MDBContainer className="p-3">
         <section className="social-media-links">
           {socialMediaLinks.map((link, index) => (
-            <MDBBtn key={index} outline color="light" floating className="m-1" href={link.href} role="button">
+            <MDBBtn key={index} outline color="light" floating className="m-1" href={link.href} role="button" target="_blank" rel="noopener noreferrer">
               <MDBIcon fab icon={link.icon} />
             </MDBBtn>
           ))}

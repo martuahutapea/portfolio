3bf583fba30f672677440c50998b1d85762b8e78
martuahutapea <mhutapea2708@gmail.com>
2024-06-29 01:00:53 +0700

    Edit the projects styles and ready to edit the project picture

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,25 +1,31 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { Animate } from "react-simple-animate";
+import { IoMdDownload } from "react-icons/io";
+import Resume from "../../assets/Martua-Hutapea-resume.pdf";
+
 import "./styles.scss";
 
 const Home = ({ name }) => {
   const navigate = useNavigate();
-  console.log(navigate);
+
   const handleNavigateToContactMePage = () => {
     navigate("/contact");
   };
+
+  const handleDownloadPDF = () => {
+    // This function should handle the download logic
+    const link = document.createElement("a");
+    link.href = Resume; // Update with the actual path to your PDF file
+    link.download = "martuaresume.pdf";
+    link.click();
+  };
+
   return (
     <section id="home" className="home">
       <div className="home_text-wrapper">
-        <h1>
-          {/* Hi, {name}! */}
-          <br />
-          Welcome to my portfolio
-        </h1>
+        <h1>Welcome to my portfolio</h1>
       </div>
-
-      {/* Wrap the components inside the animate */}
 
       <Animate
         play
@@ -34,6 +40,9 @@ const Home = ({ name }) => {
       >
         <div className="home__contact-me">
           <button onClick={handleNavigateToContactMePage}>Hire Me</button>
+          <button onClick={handleDownloadPDF} className="download-button">
+            Resume <IoMdDownload />
+          </button>
         </div>
       </Animate>
     </section>
--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -6,17 +6,20 @@ import "./styles.scss";
 const projectsData = [
   {
     image: "path_to_image1.jpg",
-    description: "Project 1 Description",
+    description: "Palm Oil Management System",
+    tech: "PHP, Laravel, MongoDB",
     link: "#",
   },
   {
     image: "path_to_image2.jpg",
-    description: "Project 2 Description",
+    description: "E-commerce",
+    tech: "HTML, CSS",
     link: "#",
   },
   {
     image: "path_to_image3.jpg",
-    description: "Project 3 Description",
+    description: "Cyber Security",
+    tech: "Python",
     link: "#",
   },
 ];
@@ -24,17 +27,38 @@ const projectsData = [
 const presentProjectsData = [
   {
     image: "path_to_image4.jpg",
-    description: "Present Project 1 Description",
+    description: "Dormitory Management System",
+    tech: "PHP, JavaScript, jQuery, Bootstrap, MySQL",
     link: "#",
   },
   {
     image: "path_to_image5.jpg",
-    description: "Present Project 2 Description",
+    description: "Blog Management System",
+    tech: "PHP, JavaScript, MySQL",
     link: "#",
   },
   {
     image: "path_to_image6.jpg",
-    description: "Present Project 3 Description",
+    description: "Clone Youtube UI",
+    tech: "HTML, CSS",
+    link: "#",
+  },
+  {
+    image: "path_to_image7.jpg",
+    description: "Portfolio",
+    tech: "React.js, scss, Javascript",
+    link: "#",
+  },
+  {
+    image: "path_to_image8.jpg",
+    description: "Wedding Invitation",
+    tech: "React.js, scss, Javascript",
+    link: "#",
+  },
+  {
+    image: "path_to_image9.jpg",
+    description: "Wheather Mobile App",
+    tech: "Ionic, jQuery, Firebase",
     link: "#",
   },
 ];
@@ -43,6 +67,7 @@ const ProjectCard = ({ project }) => (
   <div className="project-card">
     <img src={project.image} alt="Project" />
     <p>{project.description}</p>
+    <p>{project.tech}</p>
     <a href={project.link} className="project-button">
       View Project
     </a>
@@ -56,7 +81,7 @@ const Projects = () => {
 
       <div className="projects-container">
         <section className="projects-section">
-          <h2>Past Projects</h2>
+          <h2>Present Projects</h2>
           <div className="projects-cards">
             {projectsData.map((project, index) => (
               <ProjectCard key={index} project={project} />
@@ -64,7 +89,7 @@ const Projects = () => {
           </div>
         </section>
         <section className="projects-section">
-          <h2>Present Projects</h2>
+          <h2>Past Projects</h2>
           <div className="projects-cards">
             {presentProjectsData.map((project, index) => (
               <ProjectCard key={index} project={project} />
